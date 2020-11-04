3ce091aec2a67e4dcef1f899a517d4e924043894
Alexander <46560544+Cmanka@users.noreply.github.com>
2020-11-04 09:57:40 +0300

    Update settings.py

--- PostOffice/PostOffice/settings.py
+++ PostOffice/PostOffice/settings.py
@@ -140,7 +140,7 @@ INTERNAL_IPS = [
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 465
-EMAIL_HOST_USER = 'andrewlabun934@gmail.com'
-EMAIL_HOST_PASSWORD = 'qwerTasdfg1234567*'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
